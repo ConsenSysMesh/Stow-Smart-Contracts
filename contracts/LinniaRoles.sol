4da8c102263b8015044e37734157b50bead8a50c
Wenchao Wang <unlock.liberty@gmail.com>
2018-01-09 20:29:26 -0500

    Add register function for doctor and provider

--- contracts/LinniaRoles.sol
+++ contracts/LinniaRoles.sol
@@ -3,6 +3,8 @@ pragma solidity 0.4.18;
 contract LinniaRoles {
     enum Role { Nil, Patient, Doctor, Provider }
     event PatientRegistered(address indexed user);
+    event DoctorRegistered(address indexed user);
+    event ProviderRegistered(address indexed user);
     event RoleUpdated(address indexed user, Role role);
 
     mapping(address => Role) public roles;
@@ -21,13 +23,28 @@ contract LinniaRoles {
         }
     }
 
-    // patientRegister allows any user to self register as a patient
-    function patientRegister() public {
+    // registerPatient allows any user to self register as a patient
+    function registerPatient() public {
         require(roles[msg.sender] == Role.Nil);
         roles[msg.sender] = Role.Patient;
         PatientRegistered(msg.sender);
     }
 
+    // registerDoctor allows admin to register a doctor
+    function registerDoctor(address user) onlyAdmin public {
+        require(roles[user] == Role.Nil);
+        roles[user] = Role.Doctor;
+        DoctorRegistered(user);
+    }
+
+    // registerProvider allows admin to register a provider
+    function registerProvider(address user) onlyAdmin public {
+        require(roles[user] == Role.Nil);
+        roles[user] = Role.Provider;
+        ProviderRegistered(user);
+    }
+
+    // updateRole allows admin to update any role
     function updateRole(address user, Role newRole) onlyAdmin public {
         roles[user] = newRole;
         RoleUpdated(user, newRole);
--- test/1_roles_test.js
+++ test/1_roles_test.js
@@ -1,49 +1,110 @@
 var LinniaRoles = artifacts.require("./LinniaRoles.sol");
 
 contract('LinniaRoles', (accounts) => {
-  it("should set the correct admin address when deployed" +
-    "with a given admin address",
-    async () => {
-      const newInstance = await LinniaRoles.new(accounts[1]);
-      const admin = await newInstance.admin()
-      assert.equal(admin, accounts[1])
+  describe("constructor", () => {
+    it("should set the correct admin address when deployed" +
+      "with a given admin address",
+      async () => {
+        const newInstance = await LinniaRoles.new(accounts[1], {
+          from: accounts[0]
+        });
+        const admin = await newInstance.admin()
+        assert.equal(admin, accounts[1])
+      })
+    it("should set the deployer as admin if not explicitly given",
+      async () => {
+        const instance = await LinniaRoles.new()
+        const admin = await instance.admin()
+        assert.equal(admin, accounts[0])
+      })
+  })
+  describe("register patient", () => {
+    it("should allow patient to self register",
+      async () => {
+        const instance = await LinniaRoles.new()
+        const tx = await instance.registerPatient({ from: accounts[1] })
+        assert.equal(tx.logs[0].args.user, accounts[1])
+        const role = await instance.roles(accounts[1])
+        assert.equal(role, 1)
+      })
+    it("should not allow a user to self register as patient twice",
+      async () => {
+        const instance = await LinniaRoles.new()
+        const tx = await instance.registerPatient({ from: accounts[1] })
+        assert.equal(tx.logs[0].args.user, accounts[1])
+        try {
+          await instance.registerPatient({ from: accounts[1] })
+          assert.fail("user has registered twice")
+        } catch (err) {
+          // ok
+        }
+      })
+  })
+  describe("register doctor", () => {
+    it("should allow admin to register a doctor", async () => {
+      const instance = await LinniaRoles.new();
+      const tx = await instance.registerDoctor(accounts[1])
+      assert.equal(tx.logs[0].args.user, accounts[1])
+      const role = await instance.roles(accounts[1])
+      assert.equal(role, 2)
     })
-  it("should set the deployer as admin if not explicitly given",
-    async () => {
-      const instance = await LinniaRoles.deployed()
-      const admin = await instance.admin()
-      assert.equal(admin, accounts[0])
-    })
-  it("should allow admin to update roles",
-    async () => {
-      const instance = await LinniaRoles.new()
-      const tx1 = await instance.updateRole(accounts[1], 1)
-      // check event
-      assert.equal(tx1.logs[0].args.user, accounts[1])
-      assert.equal(tx1.logs[0].args.role, 1)
-      // check contract state
-      const role1 = await instance.roles(accounts[1])
-      assert.equal(role1, 1)
-
-      const tx2 = await instance.updateRole(accounts[2], 2)
-      assert.equal(tx2.logs[0].args.user, accounts[2])
-      assert.equal(tx2.logs[0].args.role, 2)
-      const role2 = await instance.roles(accounts[2])
-      assert.equal(role2, 2)
-    })
-  it("should not allow non admin to update roles",
-    async () => {
-      const instance = await LinniaRoles.deployed()
+    it("should not allow non admin to register a doctor", async () => {
+      const instance = await LinniaRoles.new();
       try {
-        await instance.updateRole(accounts[1], 3, { from: accounts[1] })
+        await instance.registerDoctor(accounts[1], { from: accounts[1] })
+        assert.fail("non admin has registered a doctor")
+      } catch (err) {
+        // ok
+      }
+    })
+  })
+  describe("register provider", () => {
+    it("should allow admin to register a provider", async () => {
+      const instance = await LinniaRoles.new();
+      const tx = await instance.registerProvider(accounts[1])
+      assert.equal(tx.logs[0].args.user, accounts[1])
+      const role = await instance.roles(accounts[1])
+      assert.equal(role, 3)
+    })
+    it("should not allow non admin to register a provider", async () => {
+      const instance = await LinniaRoles.new();
+      try {
+        await instance.registerProvider(accounts[1], { from: accounts[1] })
+        assert.fail("non admin has registered a provider")
+      } catch (err) {
+        // ok
+      }
+    })
+  })
+  describe("update role", () => {
+    it("should allow admin to update an existing role", async () => {
+      const instance = await LinniaRoles.new()
+      await instance.registerPatient({ from: accounts[1] })
+      const tx1 = await instance.updateRole(accounts[1], 2)
+      assert.equal(tx1.logs[0].args.user, accounts[1])
+      assert.equal(tx1.logs[0].args.role, 2)
+      const role1 = await instance.roles(accounts[1])
+      assert.equal(role1, 2)
+    })
+    it("should not allow non admin to update roles", async () => {
+      const instance = await LinniaRoles.new()
+      try {
+        await instance.updateRole(accounts[1], 2, { from: accounts[1] })
         assert.fail("role is updated by non-admin")
       } catch (err) {
         // ok
       }
     })
-  it("should not allow non admin to change admin",
-    async () => {
-      const instance = await LinniaRoles.deployed()
+  })
+  describe("change admin", () => {
+    it("should allow admin to change admin", async () => {
+      const instance = await LinniaRoles.new({ from: accounts[0] })
+      await instance.changeAdmin(accounts[1], { from: accounts[0] })
+      const newAdmin = await instance.admin()
+      assert.equal(newAdmin, accounts[1])
+    })
+    it("should not allow non admin to change admin", async () => {
+      const instance = await LinniaRoles.new({ from: accounts[0] })
       try {
         await instance.updateRole(accounts[1], { from: accounts[1] })
         assert.fail("admin is changed by non-admin")
@@ -51,31 +112,5 @@ contract('LinniaRoles', (accounts) => {
         // ok
       }
     })
-  it("should allow admin to change admin",
-    async () => {
-      const instance = await LinniaRoles.new()
-      await instance.changeAdmin(accounts[1], { from: accounts[0] })
-      const newAdmin = await instance.admin()
-      assert.equal(newAdmin, accounts[1])
-    })
-  it("should allow patient to self register",
-    async () => {
-      const instance = await LinniaRoles.new()
-      const tx = await instance.patientRegister({ from: accounts[1] })
-      assert.equal(tx.logs[0].args.user, accounts[1])
-      const role = await instance.roles(accounts[1])
-      assert.equal(role, 1)
-    })
-  it("should disallow user to self register as patient twice",
-    async () => {
-      const instance = await LinniaRoles.new()
-      const tx = await instance.patientRegister({ from: accounts[1] })
-      assert.equal(tx.logs[0].args.user, accounts[1])
-      try {
-        await instance.patientRegister({ from: accounts[1] })
-        assert.fail("user has registered twice")
-      } catch (err) {
-        // ok
-      }
-    })
+  })
 })
