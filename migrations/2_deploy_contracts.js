b2e55fef917b65b562a0c9960c943cd1abea48b1
libertylocked <unlock.liberty@gmail.com>
2018-01-16 18:56:45 -0500

    Add LinniaRecords to deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const LinniaRoles = artifacts.require("./LinniaRoles.sol");
+const LinniaRecords = artifacts.require("./LinniaRecords.sol");
 
-module.exports = function(deployer) {
+module.exports = async (deployer, network, accounts) => {
   deployer.deploy(LinniaRoles);
+  const rolesInstance = await LinniaRoles.deployed();
+  const adminAddress = await rolesInstance.admin();
+  deployer.deploy(LinniaRecords, rolesInstance.address, adminAddress);
 };
