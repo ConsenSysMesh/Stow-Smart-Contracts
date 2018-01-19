f718e8c344bfc3445bcf27d4b38d172a84b9b032
libertylocked <unlock.liberty@gmail.com>
2018-01-19 02:26:35 -0500

    Save records artifact

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
 const LinniaRoles = artifacts.require("./LinniaRoles.sol");
 const LinniaRecords = artifacts.require("./LinniaRecords.sol");
 
-module.exports = async (deployer, network, accounts) => {
-  await deployer.deploy(LinniaRoles);
-  const rolesInstance = await LinniaRoles.deployed();
-  const adminAddress = await rolesInstance.admin();
-  deployer.deploy(LinniaRecords, rolesInstance.address, adminAddress);
-};
+module.exports = (deployer) => {
+  let rolesInstance
+  deployer.deploy(LinniaRoles).then(() => {
+    return LinniaRoles.deployed()
+  }).then((_rolesInstance) => {
+    rolesInstance = _rolesInstance
+    return rolesInstance.admin()
+  }).then((adminAddress) => {
+    return deployer.deploy(LinniaRecords, rolesInstance.address, adminAddress)
+  })
+}
