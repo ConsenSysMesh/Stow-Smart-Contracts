1706586405e60918ebda1d2bd102f1dad351b3a4
libertylocked <unlock.liberty@gmail.com>
2018-01-19 02:10:57 -0500

    Fix roles contract migration fail due to "network error"

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const LinniaRoles = artifacts.require("./LinniaRoles.sol");
 const LinniaRecords = artifacts.require("./LinniaRecords.sol");
 
 module.exports = async (deployer, network, accounts) => {
-  deployer.deploy(LinniaRoles);
+  await deployer.deploy(LinniaRoles);
   const rolesInstance = await LinniaRoles.deployed();
   const adminAddress = await rolesInstance.admin();
   deployer.deploy(LinniaRecords, rolesInstance.address, adminAddress);
