bf75b0d82e0e23e241908634abc9787c97082c0d
Wenchao Wang <unlock.liberty@gmail.com>
2018-02-19 17:38:59 -0500

    Move HTH into Records

--- test/1_hub_test.js
+++ test/1_hub_test.js
@@ -13,10 +13,9 @@ contract("LinniaHub", (accounts) => {
       const instance = await LinniaHub.new()
       assert.equal(await instance.owner(), accounts[0])
     })
-    it("should initialize roles, hth, records addresss to zero",
+    it("should initialize roles, records addresss to zero",
       async () => {
         assert.equal(await instance.rolesContract(), 0)
-        assert.equal(await instance.hthContract(), 0)
         assert.equal(await instance.recordsContract(), 0)
       })
   })
@@ -29,14 +28,6 @@ contract("LinniaHub", (accounts) => {
     })
     it("should not allow non-admin to set Roles address", async () => {
       await expectThrow(instance.setRolesContract(42, { from: accounts[1] }))
-    })
-  })
-  describe("set HTH contract", () => {
-    it("should allow admin to set HTH address", async () => {
-      const tx = await instance.setHTHContract(42)
-      assert.equal(tx.logs[0].args.from, 0)
-      assert.equal(tx.logs[0].args.to, 42)
-      assert.equal(await instance.hthContract(), 42)
     })
   })
   describe("set Records contract", () => {
