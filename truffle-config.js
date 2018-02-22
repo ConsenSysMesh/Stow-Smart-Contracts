848a5876b06d2175f41276e170f0b124c8c7dde3
Wenchao Wang <unlock.liberty@gmail.com>
2018-02-22 11:24:36 -0500

    Add solidity-coverage

--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,13 @@ module.exports = {
       port: 7545,
       gas: 6000000,
       network_id: 5777
-    }
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
   }
 };
