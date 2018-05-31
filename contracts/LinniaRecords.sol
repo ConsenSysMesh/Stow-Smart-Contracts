4741eff0186d356621b6394899c5443388f0fa31
David Rhodus <david.rhodus859@gmail.com>
2018-05-31 16:06:14 -0700

    adjust quotes for code style consistency

--- contracts/LinniaRecords.sol
+++ contracts/LinniaRecords.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./LinniaHub.sol";
 import "./LinniaUsers.sol";
 
