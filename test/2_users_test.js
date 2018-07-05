0d58905374ce3d1db6f076250158e6fb19effee3
Godfrey Hobbs <godfreyhobbs@users.noreply.github.com>
2018-07-05 11:50:25 -0600

    just prettier *.js --write --single-quote (#62)
    * prettier *.js  --write --single-quote
    
     No other changes

--- test/2_users_test.js
+++ test/2_users_test.js
@@ -1,143 +1,142 @@
-const LinniaHub = artifacts.require("./LinniaHub.sol")
-const LinniaUsers = artifacts.require("./LinniaUsers.sol")
+const LinniaHub = artifacts.require('./LinniaHub.sol');
+const LinniaUsers = artifacts.require('./LinniaUsers.sol');
 
-import assertRevert from "openzeppelin-solidity/test/helpers/assertRevert"
+import assertRevert from 'openzeppelin-solidity/test/helpers/assertRevert';
 
-contract("LinniaUsers", (accounts) => {
-  let hub
-  let instance
-  before("set up a LinniaHub contract", async () => {
-    hub = await LinniaHub.new()
-  })
-  beforeEach("deploy a new LinniaUsers contract", async () => {
-    instance = await LinniaUsers.new(hub.address)
-    await hub.setUsersContract(instance.address)
-  })
-  describe("constructor", () => {
-    it("should set the deployer as admin",
-      async () => {
-        const instance = await LinniaUsers.new(hub.address)
-        assert.equal(await instance.owner(), accounts[0])
-      })
-    it("should set hub address correctly", async () => {
-      const instance = await LinniaUsers.new(hub.address)
-      assert.equal(await instance.hub(), hub.address)
-    })
-  })
-  describe("change admin", () => {
-    it("should allow admin to change admin", async () => {
-      const instance = await LinniaUsers.new(hub.address)
-      await instance.transferOwnership(accounts[1], { from: accounts[0] })
-      assert.equal(await instance.owner(), accounts[1])
-    })
-    it("should not allow non admin to change admin", async () => {
-      const instance = await LinniaUsers.new(hub.address)
-      await assertRevert(instance.transferOwnership(accounts[1], {
-        from: accounts[1]
-      }))
-    })
-  })
-  describe("register", () => {
-    it("should allow user to self register",
-      async () => {
-        const tx = await instance.register({ from: accounts[1] })
-        assert.equal(tx.logs[0].event, "LogUserRegistered")
-        assert.equal(tx.logs[0].args.user, accounts[1])
+contract('LinniaUsers', accounts => {
+  let hub;
+  let instance;
+  before('set up a LinniaHub contract', async () => {
+    hub = await LinniaHub.new();
+  });
+  beforeEach('deploy a new LinniaUsers contract', async () => {
+    instance = await LinniaUsers.new(hub.address);
+    await hub.setUsersContract(instance.address);
+  });
+  describe('constructor', () => {
+    it('should set the deployer as admin', async () => {
+      const instance = await LinniaUsers.new(hub.address);
+      assert.equal(await instance.owner(), accounts[0]);
+    });
+    it('should set hub address correctly', async () => {
+      const instance = await LinniaUsers.new(hub.address);
+      assert.equal(await instance.hub(), hub.address);
+    });
+  });
+  describe('change admin', () => {
+    it('should allow admin to change admin', async () => {
+      const instance = await LinniaUsers.new(hub.address);
+      await instance.transferOwnership(accounts[1], { from: accounts[0] });
+      assert.equal(await instance.owner(), accounts[1]);
+    });
+    it('should not allow non admin to change admin', async () => {
+      const instance = await LinniaUsers.new(hub.address);
+      await assertRevert(
+        instance.transferOwnership(accounts[1], {
+          from: accounts[1]
+        })
+      );
+    });
+  });
+  describe('register', () => {
+    it('should allow user to self register', async () => {
+      const tx = await instance.register({ from: accounts[1] });
+      assert.equal(tx.logs[0].event, 'LogUserRegistered');
+      assert.equal(tx.logs[0].args.user, accounts[1]);
 
-        const storedUser = await instance.users(accounts[1])
-        assert.equal(storedUser[0], true)
-        assert.equal(storedUser[1], tx.receipt.blockNumber)
-        assert.equal(storedUser[2], 0)
-      })
-    it("should not allow a user to self register as twice",
-      async () => {
-        const tx = await instance.register({ from: accounts[1] })
-        assert.equal(tx.logs[0].args.user, accounts[1])
-        await assertRevert(
-          instance.register({ from: accounts[1] })
-        )
-      })
-  })
-  describe("set provenance", () => {
-    it("should allow admin to set provenance of a user", async () => {
+      const storedUser = await instance.users(accounts[1]);
+      assert.equal(storedUser[0], true);
+      assert.equal(storedUser[1], tx.receipt.blockNumber);
+      assert.equal(storedUser[2], 0);
+    });
+    it('should not allow a user to self register as twice', async () => {
+      const tx = await instance.register({ from: accounts[1] });
+      assert.equal(tx.logs[0].args.user, accounts[1]);
+      await assertRevert(instance.register({ from: accounts[1] }));
+    });
+  });
+  describe('set provenance', () => {
+    it('should allow admin to set provenance of a user', async () => {
       // register a user first
-      await instance.register({ from: accounts[1] })
+      await instance.register({ from: accounts[1] });
       // set provenance
-      const tx = await instance.setProvenance(accounts[1], 42, { from: accounts[0] })
+      const tx = await instance.setProvenance(accounts[1], 42, {
+        from: accounts[0]
+      });
       // check logs
-      assert.equal(tx.logs[0].event, "LogProvenanceChanged")
-      assert.equal(tx.logs[0].args.user, accounts[1])
-      assert.equal(tx.logs[0].args.provenance, 42)
+      assert.equal(tx.logs[0].event, 'LogProvenanceChanged');
+      assert.equal(tx.logs[0].args.user, accounts[1]);
+      assert.equal(tx.logs[0].args.provenance, 42);
       // check state
-      assert.equal((await instance.users(accounts[1]))[2], 42)
-    })
-    it("should not allow non admin to change provenance", async () => {
-      await instance.register({ from: accounts[1] })
+      assert.equal((await instance.users(accounts[1]))[2], 42);
+    });
+    it('should not allow non admin to change provenance', async () => {
+      await instance.register({ from: accounts[1] });
       await assertRevert(
         instance.setProvenance(accounts[1], 42, { from: accounts[1] })
-      )
-    })
-    it("should not allow admin to change provenance of nonexistent provider", async () => {
-      await assertRevert(
-        instance.setProvenance(accounts[1], 42)
-      )
-    })
-  })
-  describe("is user", () => {
-    it("should return true if user is registered", async () => {
-      await instance.register({ from: accounts[1] })
-      assert.equal(await instance.isUser(accounts[1]), true)
-    })
-    it("should return false if user is not registered", async () => {
-      assert.equal(await instance.isUser(accounts[1]), false)
-    })
-  })
-  describe("provenance score", () => {
-    it("should return the provenance score of a user", async () => {
-      await instance.register({ from: accounts[1] })
-      await instance.setProvenance(accounts[1], 42, { from: accounts[0] })
-      assert.equal((await instance.provenanceOf(accounts[1])).toString(),
-        "42");
-    })
+      );
+    });
+    it(
+      'should not allow admin to change provenance of nonexistent provider',
+      async () => {
+        await assertRevert(instance.setProvenance(accounts[1], 42));
+      }
+    );
+  });
+  describe('is user', () => {
+    it('should return true if user is registered', async () => {
+      await instance.register({ from: accounts[1] });
+      assert.equal(await instance.isUser(accounts[1]), true);
+    });
+    it('should return false if user is not registered', async () => {
+      assert.equal(await instance.isUser(accounts[1]), false);
+    });
+  });
+  describe('provenance score', () => {
+    it('should return the provenance score of a user', async () => {
+      await instance.register({ from: accounts[1] });
+      await instance.setProvenance(accounts[1], 42, { from: accounts[0] });
+      assert.equal((await instance.provenanceOf(accounts[1])).toString(), '42');
+    });
     it("should return 0 if user isn't registered", async () => {
-      assert.equal(await instance.provenanceOf(accounts[1]), 0)
-    })
-  })
-  describe("pausable", () => {
-    it("should not allow non-admin to pause or unpause", async () => {
-      await assertRevert(instance.pause({ from: accounts[1] }))
-      await assertRevert(instance.unpause({ from: accounts[1] }))
-    })
-    it("should not allow register of users when paused by admin", async () => {
-      const tx = await instance.pause()
-      assert.equal(await instance.isUser(accounts[1]), false)
-      assert.equal(tx.logs[0].event, "Pause")
-      await assertRevert(instance.register({ from: accounts[1] }))
-      const tx2 = await instance.unpause()
-      assert.equal(tx2.logs[0].event, "Unpause")
-      const tx3 = await instance.register({ from: accounts[1] })
-      assert.equal(tx3.logs[0].event, "LogUserRegistered")
-    })
-  })
+      assert.equal(await instance.provenanceOf(accounts[1]), 0);
+    });
+  });
+  describe('pausable', () => {
+    it('should not allow non-admin to pause or unpause', async () => {
+      await assertRevert(instance.pause({ from: accounts[1] }));
+      await assertRevert(instance.unpause({ from: accounts[1] }));
+    });
+    it('should not allow register of users when paused by admin', async () => {
+      const tx = await instance.pause();
+      assert.equal(await instance.isUser(accounts[1]), false);
+      assert.equal(tx.logs[0].event, 'Pause');
+      await assertRevert(instance.register({ from: accounts[1] }));
+      const tx2 = await instance.unpause();
+      assert.equal(tx2.logs[0].event, 'Unpause');
+      const tx3 = await instance.register({ from: accounts[1] });
+      assert.equal(tx3.logs[0].event, 'LogUserRegistered');
+    });
+  });
   // copy paste from records contract
-  describe("destructible", () => {
-    it("should not allow non-admin to destroy", async () => {
-      await assertRevert(instance.destroy({ from: accounts[1] }))
-    })
-    it("should allow admin to destroy", async () => {
-      const admin = accounts[0]
-      assert.notEqual(web3.eth.getCode(instance.address), '0x0')
-      const tx = await instance.destroy({from: admin})
-      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`)
-      assert.equal(web3.eth.getCode(instance.address), '0x0')
-    })
-    it("should allow admin to destroyAndSend", async () => {
-      const admin = accounts[0]
-      assert.notEqual(web3.eth.getCode(instance.address), '0x0')
-      const tx = await instance.destroyAndSend(admin, {from: admin})
-      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`)
-      assert.equal(web3.eth.getCode(instance.address), '0x0')
-      assert.equal(web3.eth.getBalance(instance.address).toNumber(),0)
-    })
-  })
-})
+  describe('destructible', () => {
+    it('should not allow non-admin to destroy', async () => {
+      await assertRevert(instance.destroy({ from: accounts[1] }));
+    });
+    it('should allow admin to destroy', async () => {
+      const admin = accounts[0];
+      assert.notEqual(web3.eth.getCode(instance.address), '0x0');
+      const tx = await instance.destroy({ from: admin });
+      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`);
+      assert.equal(web3.eth.getCode(instance.address), '0x0');
+    });
+    it('should allow admin to destroyAndSend', async () => {
+      const admin = accounts[0];
+      assert.notEqual(web3.eth.getCode(instance.address), '0x0');
+      const tx = await instance.destroyAndSend(admin, { from: admin });
+      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`);
+      assert.equal(web3.eth.getCode(instance.address), '0x0');
+      assert.equal(web3.eth.getBalance(instance.address).toNumber(), 0);
+    });
+  });
+});
--- test/4_permissions_test.js
+++ test/4_permissions_test.js
@@ -1,173 +1,207 @@
-const LinniaHub = artifacts.require("./LinniaHub.sol")
-const LinniaUsers = artifacts.require("./LinniaUsers.sol")
-const LinniaRecords = artifacts.require("./LinniaRecords.sol")
-const LinniaPermissions = artifacts.require("./LinniaPermissions.sol")
+const LinniaHub = artifacts.require('./LinniaHub.sol');
+const LinniaUsers = artifacts.require('./LinniaUsers.sol');
+const LinniaRecords = artifacts.require('./LinniaRecords.sol');
+const LinniaPermissions = artifacts.require('./LinniaPermissions.sol');
 
-const bs58 = require("bs58")
-const crypto = require("crypto")
-const eutil = require("ethereumjs-util")
-const multihashes = require("multihashes")
+const bs58 = require('bs58');
+const crypto = require('crypto');
+const eutil = require('ethereumjs-util');
+const multihashes = require('multihashes');
 
-import assertRevert from "openzeppelin-solidity/test/helpers/assertRevert"
+import assertRevert from 'openzeppelin-solidity/test/helpers/assertRevert';
 
-const testDataContent1 = `{"foo":"bar","baz":42}`
-const testDataContent2 = `{"asdf":42}`
-const testDataHash1 = eutil.bufferToHex(eutil.sha3(testDataContent1))
-const testDataHash2 = eutil.bufferToHex(eutil.sha3(testDataContent2))
-const testDataUri1 = "QmUMqi1rr4Ad1eZ3ctsRUEmqK2U3CyZqpetUe51LB9GiAM"
-const testDataUri2 = "QmUoCHEZqSuYhr9fV1c2b4gLASG2hPpC2moQXQ6qzy697d"
-const testMetadata = "KEYWORDS"
+const testDataContent1 = `{"foo":"bar","baz":42}`;
+const testDataContent2 = `{"asdf":42}`;
+const testDataHash1 = eutil.bufferToHex(eutil.sha3(testDataContent1));
+const testDataHash2 = eutil.bufferToHex(eutil.sha3(testDataContent2));
+const testDataUri1 = 'QmUMqi1rr4Ad1eZ3ctsRUEmqK2U3CyZqpetUe51LB9GiAM';
+const testDataUri2 = 'QmUoCHEZqSuYhr9fV1c2b4gLASG2hPpC2moQXQ6qzy697d';
+const testMetadata = 'KEYWORDS';
 
-contract("LinniaPermissions", (accounts) => {
-  const admin = accounts[0]
-  const patient1 = accounts[1]
-  const patient2 = accounts[2]
-  const provider1 = accounts[3]
-  const provider2 = accounts[4]
-  let hub, instance
+contract('LinniaPermissions', accounts => {
+  const admin = accounts[0];
+  const patient1 = accounts[1];
+  const patient2 = accounts[2];
+  const provider1 = accounts[3];
+  const provider2 = accounts[4];
+  let hub, instance;
 
-  before("set up a LinniaHub contract", async () => {
-    hub = await LinniaHub.new()
-  })
-  before("set up a LinniaUsers contract", async () => {
-    const usersInstance = await LinniaUsers.new(hub.address)
-    await hub.setUsersContract(usersInstance.address)
-    usersInstance.register({ from: patient1 })
-    usersInstance.register({ from: patient2 })
-    usersInstance.register({ from: provider1 })
-    usersInstance.register({ from: provider2 })
-    usersInstance.setProvenance(provider1, 1, { from: admin })
-    usersInstance.setProvenance(provider2, 1, { from: admin })
-  })
-  before("set up a LinniaRecords contract", async () => {
-    const recordsInstance = await LinniaRecords.new(hub.address)
-    await hub.setRecordsContract(recordsInstance.address)
+  before('set up a LinniaHub contract', async () => {
+    hub = await LinniaHub.new();
+  });
+  before('set up a LinniaUsers contract', async () => {
+    const usersInstance = await LinniaUsers.new(hub.address);
+    await hub.setUsersContract(usersInstance.address);
+    usersInstance.register({ from: patient1 });
+    usersInstance.register({ from: patient2 });
+    usersInstance.register({ from: provider1 });
+    usersInstance.register({ from: provider2 });
+    usersInstance.setProvenance(provider1, 1, { from: admin });
+    usersInstance.setProvenance(provider2, 1, { from: admin });
+  });
+  before('set up a LinniaRecords contract', async () => {
+    const recordsInstance = await LinniaRecords.new(hub.address);
+    await hub.setRecordsContract(recordsInstance.address);
     // upload 2 records, one for patient1 and one for patient2
     // 1st one is not attested, 2nd one is attested by provider1
-    await recordsInstance.addRecord(testDataHash1,
-      testMetadata, testDataUri1, { from: patient1 })
-    await recordsInstance.addRecordByProvider(testDataHash2,
-      patient2, testMetadata, testDataUri2, { from: provider1 })
-  })
-  beforeEach("deploy a new LinniaPermissions contract", async () => {
-    instance = await LinniaPermissions.new(hub.address,
-      { from: accounts[0] })
-    await hub.setPermissionsContract(instance.address)
-  })
+    await recordsInstance.addRecord(testDataHash1, testMetadata, testDataUri1, {
+      from: patient1
+    });
+    await recordsInstance.addRecordByProvider(
+      testDataHash2,
+      patient2,
+      testMetadata,
+      testDataUri2,
+      { from: provider1 }
+    );
+  });
+  beforeEach('deploy a new LinniaPermissions contract', async () => {
+    instance = await LinniaPermissions.new(hub.address, { from: accounts[0] });
+    await hub.setPermissionsContract(instance.address);
+  });
 
-  describe("constructor", () => {
-    it("should set hub address correctly", async () => {
-      const instance = await LinniaRecords.new(hub.address,
-        { from: accounts[0] })
-      assert.equal(await instance.hub(), hub.address)
-    })
-  })
-  describe("grant access", () => {
-    it("should allow patient to grant access to their files", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
-      const tx = await instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 })
-      assert.equal(tx.logs[0].event, "LogAccessGranted")
-      assert.equal(tx.logs[0].args.dataHash, testDataHash1)
-      assert.equal(tx.logs[0].args.owner, patient1)
-      assert.equal(tx.logs[0].args.viewer, provider2)
-      const perm = await instance.permissions(testDataHash1, provider2)
-      assert.equal(perm[0], true)
-      assert.equal(perm[1], fakeIpfsHash)
-    })
-    it("should not allow non-owner to grant access", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
+  describe('constructor', () => {
+    it('should set hub address correctly', async () => {
+      const instance = await LinniaRecords.new(hub.address, {
+        from: accounts[0]
+      });
+      assert.equal(await instance.hub(), hub.address);
+    });
+  });
+  describe('grant access', () => {
+    it('should allow patient to grant access to their files', async () => {
+      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
+      const tx = await instance.grantAccess(
+        testDataHash1,
+        provider2,
+        fakeIpfsHash,
+        { from: patient1 }
+      );
+      assert.equal(tx.logs[0].event, 'LogAccessGranted');
+      assert.equal(tx.logs[0].args.dataHash, testDataHash1);
+      assert.equal(tx.logs[0].args.owner, patient1);
+      assert.equal(tx.logs[0].args.viewer, provider2);
+      const perm = await instance.permissions(testDataHash1, provider2);
+      assert.equal(perm[0], true);
+      assert.equal(perm[1], fakeIpfsHash);
+    });
+    it('should not allow non-owner to grant access', async () => {
+      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
       await assertRevert(
-        instance.grantAccess(testDataHash1, provider2,
-          fakeIpfsHash, { from: provider1 })
-      )
+        instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+          from: provider1
+        })
+      );
       await assertRevert(
-        instance.grantAccess(testDataHash1, provider2,
-          fakeIpfsHash, { from: provider2 })
-      )
+        instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+          from: provider2
+        })
+      );
       await assertRevert(
-        instance.grantAccess(testDataHash1, provider2,
-          fakeIpfsHash, { from: patient2 })
-      )
-    })
-    it("should reject if viewer or data uri is zero", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
-      await assertRevert(instance.grantAccess(testDataHash1,
-        0, fakeIpfsHash, { from: patient1 }))
-      await assertRevert(instance.grantAccess(testDataHash1,
-        provider2, 0, { from: patient1 }))
-    })
-    it("should not allow sharing same record twice with same user", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
-      await instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 })
-      await assertRevert(instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 }))
-    })
-  })
-  describe("revoke access", () => {
+        instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+          from: patient2
+        })
+      );
+    });
+    it('should reject if viewer or data uri is zero', async () => {
+      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
+      await assertRevert(
+        instance.grantAccess(testDataHash1, 0, fakeIpfsHash, { from: patient1 })
+      );
+      await assertRevert(
+        instance.grantAccess(testDataHash1, provider2, 0, { from: patient1 })
+      );
+    });
+    it(
+      'should not allow sharing same record twice with same user',
+      async () => {
+        const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
+        await instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+          from: patient1
+        });
+        await assertRevert(
+          instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+            from: patient1
+          })
+        );
+      }
+    );
+  });
+  describe('revoke access', () => {
     beforeEach("grant provider2 to access patient1's record1", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
-      await instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 })
-    })
-    it("should allow owner to revoke access to their files", async () => {
-      const tx = await instance.revokeAccess(testDataHash1,
-        provider2, { from: patient1 })
-      assert.equal(tx.logs[0].event, "LogAccessRevoked")
-      assert.equal(tx.logs[0].args.dataHash, testDataHash1)
-      assert.equal(tx.logs[0].args.owner, patient1)
-      assert.equal(tx.logs[0].args.viewer, provider2)
-      const perm = await instance.permissions(testDataHash1, provider2)
-      assert.equal(perm[0], false)
-      assert.equal(perm[1], "")
-    })
-    it("should not allow non-owner to revoke access to files", async () => {
-      await assertRevert(instance.revokeAccess(testDataHash1,
-        provider2, { from: provider1 }))
-      await assertRevert(instance.revokeAccess(testDataHash1,
-        provider2, { from: provider2 }))
-      await assertRevert(instance.revokeAccess(testDataHash1,
-        provider2, { from: patient2 }))
-    })
-  })
-  describe("pausable", () => {
-    it("should not allow non-admin to pause or unpause", async () => {
-      await assertRevert(instance.pause({ from: accounts[1] }))
-      await assertRevert(instance.unpause({ from: accounts[1] }))
-    })
-    it("should not allow sharing records when paused by admin", async () => {
-      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32))
-      const tx = await instance.pause()
-      assert.equal(tx.logs[0].event, "Pause")
-      await assertRevert(instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 }))
-      const tx2 = await instance.unpause()
-      assert.equal(tx2.logs[0].event, "Unpause")
-      const tx3 = await instance.grantAccess(testDataHash1, provider2,
-        fakeIpfsHash, { from: patient1 })
-      assert.equal(tx3.logs[0].event, "LogAccessGranted")
-    })
-  })
+      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
+      await instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+        from: patient1
+      });
+    });
+    it('should allow owner to revoke access to their files', async () => {
+      const tx = await instance.revokeAccess(testDataHash1, provider2, {
+        from: patient1
+      });
+      assert.equal(tx.logs[0].event, 'LogAccessRevoked');
+      assert.equal(tx.logs[0].args.dataHash, testDataHash1);
+      assert.equal(tx.logs[0].args.owner, patient1);
+      assert.equal(tx.logs[0].args.viewer, provider2);
+      const perm = await instance.permissions(testDataHash1, provider2);
+      assert.equal(perm[0], false);
+      assert.equal(perm[1], '');
+    });
+    it('should not allow non-owner to revoke access to files', async () => {
+      await assertRevert(
+        instance.revokeAccess(testDataHash1, provider2, { from: provider1 })
+      );
+      await assertRevert(
+        instance.revokeAccess(testDataHash1, provider2, { from: provider2 })
+      );
+      await assertRevert(
+        instance.revokeAccess(testDataHash1, provider2, { from: patient2 })
+      );
+    });
+  });
+  describe('pausable', () => {
+    it('should not allow non-admin to pause or unpause', async () => {
+      await assertRevert(instance.pause({ from: accounts[1] }));
+      await assertRevert(instance.unpause({ from: accounts[1] }));
+    });
+    it('should not allow sharing records when paused by admin', async () => {
+      const fakeIpfsHash = eutil.bufferToHex(crypto.randomBytes(32));
+      const tx = await instance.pause();
+      assert.equal(tx.logs[0].event, 'Pause');
+      await assertRevert(
+        instance.grantAccess(testDataHash1, provider2, fakeIpfsHash, {
+          from: patient1
+        })
+      );
+      const tx2 = await instance.unpause();
+      assert.equal(tx2.logs[0].event, 'Unpause');
+      const tx3 = await instance.grantAccess(
+        testDataHash1,
+        provider2,
+        fakeIpfsHash,
+        { from: patient1 }
+      );
+      assert.equal(tx3.logs[0].event, 'LogAccessGranted');
+    });
+  });
   // copy paste from records contract
-  describe("destructible", () => {
-    it("should not allow non-admin to destroy", async () => {
-      await assertRevert(instance.destroy({ from: accounts[1] }))
-    })
-    it("should allow admin to destroy", async () => {
-      const admin = accounts[0]
-      assert.notEqual(web3.eth.getCode(instance.address), '0x0')
-      const tx = await instance.destroy({from: admin})
-      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`)
-      assert.equal(web3.eth.getCode(instance.address), '0x0')
-    })
-    it("should allow admin to destroyAndSend", async () => {
-      const admin = accounts[0]
-      assert.notEqual(web3.eth.getCode(instance.address), '0x0')
-      const tx = await instance.destroyAndSend(admin, {from: admin})
-      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`)
-      assert.equal(web3.eth.getCode(instance.address), '0x0')
-      assert.equal(web3.eth.getBalance(instance.address).toNumber(),0)
-    })
-  })
-})
+  describe('destructible', () => {
+    it('should not allow non-admin to destroy', async () => {
+      await assertRevert(instance.destroy({ from: accounts[1] }));
+    });
+    it('should allow admin to destroy', async () => {
+      const admin = accounts[0];
+      assert.notEqual(web3.eth.getCode(instance.address), '0x0');
+      const tx = await instance.destroy({ from: admin });
+      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`);
+      assert.equal(web3.eth.getCode(instance.address), '0x0');
+    });
+    it('should allow admin to destroyAndSend', async () => {
+      const admin = accounts[0];
+      assert.notEqual(web3.eth.getCode(instance.address), '0x0');
+      const tx = await instance.destroyAndSend(admin, { from: admin });
+      assert.equal(tx.logs.length, 0, `did not expect logs but got ${tx.logs}`);
+      assert.equal(web3.eth.getCode(instance.address), '0x0');
+      assert.equal(web3.eth.getBalance(instance.address).toNumber(), 0);
+    });
+  });
+});
